51f99c082c993d72087ea57f47b2ce22869f9b05
amira <amiraabouhadid@gmail.com>
2021-03-25 18:40:43 +0200

    add logic files in lib

--- bin/main.rb
+++ bin/main.rb
@@ -6,12 +6,18 @@ class TicGame
     puts 'Welcome to Ruby\'s Tic Tac Toe!'
     player1 = Player.new(name('X'))
     player2 = Player.new(name('O'))
+    puts "#{player1.name} will play with X and #{player2.name} will play with O"
     @game = Game.new(player1, player2)
     game_round
   end
 
   def name(token)
-    puts 'Please enter player name:'
+    case token
+    when 'X'
+      puts 'First player, please enter your name:'
+    when 'O'
+      puts 'Second player, please enter your name:'
+    end
     name = gets.chomp
     name(token) if name.empty?
     name
@@ -23,23 +29,22 @@ class TicGame
       @board.show
       @game.assign_current_player
       @game.turn
+      @game.count
       select_position
       if @game.winner
         @game.game_over(@game.winner)
         @board.show
         exit
-      elsif @game.turn == 9
+      elsif @game.count == 9
         @game.game_over(nil)
         @board.show
         exit
       end
     end
     reset
-    # while @game.turn_count <= 9 || @game.win
   end
 
   def select_position
-    puts "#{@game.assign_current_player.name}, select a position 1-9:"
     position = gets.chomp.to_i - 1
     if @game.valid_move?(position)
       @game.move(position)
