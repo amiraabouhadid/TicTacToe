bbb4ab3eca81019fc759cb44ed2eff43c8c4b579
amira <amiraabouhadid@gmail.com>
2021-03-25 12:31:33 +0200

    Add Ticgame class

--- bin/main.rb
+++ bin/main.rb
@@ -16,49 +16,47 @@ end
 class Game
   attr_reader :current_player
 
-  def initialize
+  def initialize(player1, player2)
     puts 'Welcome to Ruby\'s Tic Tac Toe'
     @board = Board.new
     @player1 = player1
     @player2 = player2
-    player1 = Player.new(name('X'))
-    player2 = Player.new(name('O'))
-    @game = Game.new(player1, player2)
-    game_round
   end
 end
 
-def name(player)
-  puts 'Enter player name :'
-  name = gets.chomp
-  name(player) if name.empty?
-  name
-end
+class TicGame
+  def name(player)
+    puts 'Enter player name :'
+    name = gets.chomp
+    name(player) if name.empty?
+    name
+  end
 
-def game_round
-  loop do
-    puts "It's #{@game.current_player}'s turn!'"
-    puts @game.turn
-    puts @game.board
-    player_input
-    if @game.end?
-      puts @game.game_over(@game.end)
-    else
-      puts @game.game_over(nil)
+  def game_round
+    loop do
+      puts "It's #{@game.current_player}'s turn!'"
+      puts @game.turn
+      puts @game.board
+      player_input
+      if @game.end?
+        puts @game.game_over(@game.end)
+      else
+        puts @game.game_over(nil)
+      end
+      puts @game.board
     end
-    puts @game.board
+  end
+
+  def player_input
+    puts "#{@game.current_player.name}, select a position 1-9:"
+    position = gets.chomp.to_i - 1
+    if @game.valid_move?(position)
+      @game.move(position)
+    else
+      puts 'Invalid move, please enter a valid position.'
+      player_input
+    end
   end
 end
 
-def player_input
-  puts "#{@game.current_player.name}, select a position 1-9:"
-  position = gets.chomp.to_i - 1
-  if @game.valid_move?(position)
-    @game.move(position)
-  else
-    puts 'Invalid move, please enter a valid position.'
-    player_input
-  end
-end
-
-Game.new
+TicGame.new
