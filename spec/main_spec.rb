523b29b19c0a68af10ccf5c8b932207617428252
amira <amiraabouhadid@gmail.com>
2021-03-29 11:25:58 +0200

    add #valid_move? and #game_over rspec tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,18 +1,28 @@
-require_relative '../bin/main'
+require_relative '../lib/player'
+require_relative '../lib/game'
+require_relative '../lib/board'
 
-describe TicGame do
-  let(:ticgame) { TicGame.new }
-  describe '#name' do
-    it 'returns the name of player given' do
-      player1 = Player.new('amira')
-      player2 = Player.new('omar')
-      expect ticgame.name(player1).to eql('amira')
-      expect ticgame.name(player2).to eql('omar')
+player1 = Player.new('amira')
+player2 = Player.new('omar')
+game = Game.new(player1, player2)
+
+describe Game do
+  describe '#valid_move?' do
+    it 'returns true or false' do
+      expect(game.valid_move?(1)).to eql(true)
+      expect(game.valid_move?(0)).to eql(true)
+      expect(game.valid_move?(12)).to eql(false)
+      expect(game.valid_move?(10)).to eql(false)
+      expect(game.valid_move?(3)).to eql(true)
     end
   end
-  describe '#select_position' do
-    it 'returns the position the player selected' do
-      expect ticgame.select_position.to eql(position)
+
+  describe '#game_over' do
+    it 'returns string' do
+      current_player = game.assign_current_player
+      expect(game.game_over(nil)).to eql("It's a TIE")
+      expect(game.game_over(player1)).to eql("#{current_player.name} wins, GAME OVER")
+      expect(game.game_over(player2)).to eql("#{current_player.name} wins, GAME OVER")
     end
   end
 end
